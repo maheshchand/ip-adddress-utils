cd4899864162f7d9ce6685752be3ac76d5e0dd13
Mahesh Chand <mahesh.yadav.107@gmail.com>
2014-08-06 05:07:04 -0700

    Updated to work when used as cron script, added some messages

--- ip_fetch_without_login.sh
+++ ip_fetch_without_login.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
+echo "Starting to find public and local IP address"
 pIp=$(wget -qO- http://ipecho.net/plain)
-lIp=$(ifconfig | awk -F':' '/inet addr/&&!/127.0.0.1/{split($2,_," ");print _[1]}')
+lIp=$(/sbin/ifconfig | awk -F':' '/inet addr/&&!/127.0.0.1/{split($2,_," ");print _[1]}')
 
-echo $pIp
-echo $lIp
+#Enable this if above command is not able to find local IP address
+#lIp2=$(/sbin/ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')
+echo "Public IP Address: " $pIp
+echo "Local IP Address: " $lIp
 
+echo "Sending an email to you with IP address information"
 #sudo apt-get install sendemail
-sendemail -f fromemail@gmail.com -t toemail@gmail.com -u "IP Address information" -m "Public IP: $pIp \n Local IP: $lIp" -s smtp.gmail.com -o tls=yes -xu username -xp password
+sendemail -f fromemail@gmail.com -t toemail@gmail.com -u "IP Address information" -m "Public IP: $pIp \n Local IP: $lIp" -s smtp.gmail.com -o tls=yes -xu gmail-user-name -xp gmail-password
